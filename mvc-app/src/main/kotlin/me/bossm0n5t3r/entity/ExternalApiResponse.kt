7100d58e86e92b89bf7f276315bdd7442c307d3a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-08-15 09:45:00 +0900

    refactor(mvc-app): remove unnecessary `@Lob` annotations in the `ExternalApiResponse` entity

--- mvc-app/src/main/kotlin/me/bossm0n5t3r/entity/ExternalApiResponse.kt
+++ mvc-app/src/main/kotlin/me/bossm0n5t3r/entity/ExternalApiResponse.kt
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.Lob
 import jakarta.persistence.Table
 import me.bossm0n5t3r.table.ExternalApiResponseTable
 import java.time.LocalDateTime
@@ -19,19 +18,14 @@ data class ExternalApiResponse(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     override val id: Long = 0,
-    @Lob
     @Column(nullable = false)
     override val userInfo: String,
-    @Lob
     @Column(nullable = false)
     override val weatherInfo: String,
-    @Lob
     @Column(nullable = false)
     override val stockPriceInfo: String,
-    @Lob
     @Column(nullable = false)
     override val orderStatusInfo: String,
-    @Lob
     @Column(nullable = false)
     override val metricInfo: String,
     @Column(name = "created_at")
