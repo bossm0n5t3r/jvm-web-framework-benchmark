437f517c0cf7b2c0624386bffa3d2a902adc2286
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-08-23 22:00:00 +0900

    feat(benchmark-app): automate copying of Gatling reports to the project root
    - Add the `copyGatlingReports` task to copy reports to `reports/gatling` in the project root.
    - Set the `gatlingRun` task to finalize by executing `copyGatlingReports`.

--- benchmark-app/build.gradle.kts
+++ benchmark-app/build.gradle.kts
@@ -34,3 +34,15 @@ dependencies {
 tasks.bootRun {
     enabled = false
 }
+
+// Gatling 리포트를 프로젝트 루트의 'gatling-reports' 디렉터리로 복사하는 태스크를 정의합니다.
+val copyGatlingReports =
+    tasks.register<Copy>("copyGatlingReports") {
+        from(layout.buildDirectory.dir("reports/gatling"))
+        into(rootProject.layout.projectDirectory.dir("reports/gatling"))
+    }
+
+// 'gatlingRun' 태스크가 완료된 후 'copyGatlingReports' 태스크를 실행하도록 설정합니다.
+tasks.named("gatlingRun") {
+    finalizedBy(copyGatlingReports)
+}
