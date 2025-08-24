84a54ae6d51e8a06ba8a98e291a6a4e1a060d234
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-08-24 17:33:00 +0900

    feat(benchmark-app): add Gatling-generated assets for `ExternalAPIWithNoDatabaseSimulation`
    - Include HTML, JS, CSS, and SVG assets under `reports/gatling/externalapiwithnodatabasesimulation` for visualization.

---
+++ reports/gatling/externalapiwithnodatabasesimulation/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1756024550000,2],[1756024551000,2],[1756024552000,2],[1756024553000,2],[1756024554000,2],[1756024555000,2]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
