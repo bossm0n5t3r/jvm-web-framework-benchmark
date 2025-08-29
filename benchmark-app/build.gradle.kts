dbb677b4bc6d8b11b49bd572bbafc5666539e95e
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-08-29 23:00:00 +0900

    refactor(benchmark-app): clean up dependencies and update boot task configuration
    - Removed unused dependencies for Spring Boot, HTTP clients, JSON processing, and coroutines.
    - Disabled `bootJar` task instead of `bootRun`.

--- benchmark-app/build.gradle.kts
+++ benchmark-app/build.gradle.kts
@@ -9,35 +9,12 @@ dependencies {
     implementation(project(":mvc-without-virtual-thread-app"))
     implementation(project(":webflux-app"))
 
-    api(project(":common"))
-
-    // Spring Boot starter for an application framework
-    implementation(libs.spring.boot.starter)
-    implementation(libs.kotlin.reflect)
-
-    // HTTP Client dependencies for making requests
-    implementation(libs.spring.boot.starter.web) // RestClient for blocking HTTP calls
-    implementation(libs.spring.boot.starter.webflux) // WebClient for reactive HTTP calls
-
-    // JSON processing
-    implementation(libs.jackson.module.kotlin)
-
-    // Statistics and metrics
-    implementation(libs.kotlinx.dataframe)
-
-    // Coroutines for async operations
-    implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.kotlinx.coroutines.reactor)
-
     // gatling
     implementation(libs.gatling.test.framework)
     implementation(libs.gatling.charts.highcharts)
-
-    // Testing
-    testImplementation(libs.spring.boot.starter.test)
 }
 
-tasks.bootRun {
+tasks.bootJar {
     enabled = false
 }
 
