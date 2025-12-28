f48b8caa62cbd4764ea2906d0bb6d11bd6570d1d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-12-28 21:00:00 +0900

    refactor(dto): remove unused fields from `ExternalApiResponseDto` and `UserDto`
    - Removed `id`, `createdAt`, and `updatedAt` fields from both DTOs.
    - Updated table-to-DTO conversion methods accordingly.

--- common/src/main/kotlin/me/bossm0n5t3r/dto/UserDto.kt
+++ common/src/main/kotlin/me/bossm0n5t3r/dto/UserDto.kt
@@ -1,21 +1,14 @@
 package me.bossm0n5t3r.dto
 
 import me.bossm0n5t3r.table.UserTable
-import java.time.LocalDateTime
 
 data class UserDto(
-    val id: Long,
     val name: String,
     val email: String,
-    val createdAt: LocalDateTime,
-    val updatedAt: LocalDateTime,
 )
 
 fun UserTable.toDto() =
     UserDto(
-        id = this.notNullId,
         name = this.name,
         email = this.email,
-        createdAt = this.createdAt,
-        updatedAt = this.updatedAt,
     )
