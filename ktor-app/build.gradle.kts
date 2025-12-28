58dc635cc743dd72ca716376d9dbd84c5cd34545
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-12-28 22:00:00 +0900

    feat(gradle): configure `ktor-app` with dependencies, plugins, and application setup
    - Added required `Ktor`, database, and logging dependencies.
    - Configured Kotlin, Java toolchain, and ktlint.
    - Set up main class and application configuration.

--- ktor-app/build.gradle.kts
+++ ktor-app/build.gradle.kts
@@ -1,1 +1,68 @@
-dependencies {}
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.ktlint)
+    application
+}
+
+group = "me.bossm0n5t3r"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion =
+            JavaLanguageVersion.of(
+                libs.versions.jdk.version
+                    .get()
+                    .toInt(),
+            )
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api(project(":common"))
+
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.serialization.jackson)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.jackson.datatype.jsr310)
+
+    implementation(libs.hikaricp)
+    runtimeOnly(libs.postgresql)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.java.time)
+    implementation(libs.exposed.jdbc)
+
+    implementation(libs.logback.classic)
+}
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+ktlint {
+    version.set(
+        libs.versions.pinterest.ktlint
+            .get(),
+    )
+}
