82717986c5f699c2e6a3bce392f384a4a429c4db
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-01-04 03:50:00 +0900

    refactor(benchmark): streamline WebFlux simulation imports and cleanup scenario setup

--- benchmark-app/src/main/kotlin/me/bossm0n5t3r/benchmark/WebFluxSimulation.kt
+++ benchmark-app/src/main/kotlin/me/bossm0n5t3r/benchmark/WebFluxSimulation.kt
@@ -1,8 +1,11 @@
 package me.bossm0n5t3r.benchmark
 
-import io.gatling.javaapi.core.CoreDsl
+import io.gatling.javaapi.core.CoreDsl.global
+import io.gatling.javaapi.core.CoreDsl.rampUsers
+import io.gatling.javaapi.core.CoreDsl.scenario
 import io.gatling.javaapi.core.Simulation
 import io.gatling.javaapi.http.HttpDsl
+import io.gatling.javaapi.http.HttpDsl.http
 import me.bossm0n5t3r.benchmark.Constants.APPLICATION_JSON
 import me.bossm0n5t3r.benchmark.Constants.WEBFLUX_URL
 import java.time.Duration
@@ -18,17 +21,15 @@ class WebFluxSimulation : Simulation() {
             .contentTypeHeader(APPLICATION_JSON)
 
     private val scenario =
-        CoreDsl
-            .scenario("WebFlux Load Test")
+        scenario("WebFlux Load Test")
             .exec(
-                HttpDsl
-                    .http("[WebFlux] External API with no database")
+                http("[WebFlux] External API with no database")
                     .get("/webflux/external/no-db")
                     .check(HttpDsl.status().`is`(200)),
             )
 
     init {
-        val rampUsers = CoreDsl.rampUsers(users).during(Duration.ofSeconds(10))
+        val rampUsers = rampUsers(users).during(Duration.ofSeconds(10))
 
         setUp(
             scenario
@@ -36,11 +37,7 @@ class WebFluxSimulation : Simulation() {
                 .protocols(httpProtocol),
         ).maxDuration(Duration.ofMinutes(3))
             .assertions(
-                CoreDsl
-                    .global()
-                    .successfulRequests()
-                    .percent()
-                    .gt(60.0),
+                global().successfulRequests().percent().gt(60.0),
             )
     }
 }
